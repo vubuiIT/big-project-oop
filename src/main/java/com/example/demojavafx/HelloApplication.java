baca73e9035cd4d99787316edfdeb53f4bde6652
Vũ đẹp trai <anhvu200376@gmail.com>
2023-06-13 16:59:31 +0700

    Change test

--- src/main/java/com/example/demojavafx/HelloApplication.java
+++ src/main/java/com/example/demojavafx/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Main");
+        stage.setTitle("Hello cc xx");
         stage.setScene(scene);
         stage.show();
     }
