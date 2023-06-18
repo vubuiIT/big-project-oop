c8e4463be504f0fbcf907bf8047bb68ddac68476
Vũ đẹp trai <anhvu200376@gmail.com>
2023-06-18 23:33:38 +0700

    Change stage tittle

--- src/main/java/com/example/demojavafx/Main.java
+++ src/main/java/com/example/demojavafx/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("gui1-2.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello cc xx");
+        stage.setTitle("App question bank");
         stage.setScene(scene);
         stage.show();
     }
