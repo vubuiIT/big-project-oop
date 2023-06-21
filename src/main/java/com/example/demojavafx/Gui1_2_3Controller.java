aaee7fec5e66bf8448808799689478898ae00423
Bongg <trinhhoanganh.pl@gmail.com>
2023-06-21 17:32:46 +0700

    Hyperlink: categories + import + export

--- src/main/java/com/example/demojavafx/Gui1_2_3Controller.java
+++ src/main/java/com/example/demojavafx/Gui1_2_3Controller.java
@@ -1,34 +1,31 @@
 package com.example.demojavafx;
 
+import com.almasb.fxgl.entity.Entity;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.ListView;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TabPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.control.TreeView;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HelloController implements Initializable {
+public class Gui1_2_3Controller implements Initializable {
     final boolean[] isPopupVisible = {false};
-    @FXML
-    private Text default2;
 
     @FXML
     private Button Menu;
 
     @FXML
     private Button MenuBack;
-
-    @FXML
-    private Button create1;
 
     @FXML
     private Spinner<?> default1;
@@ -48,16 +45,70 @@ public class HelloController implements Initializable {
     @FXML
     private GridPane slider;
 
-    @FXML
-    private CheckBox tick1;
+    final boolean[] isTab2Popup = {false};
 
+    @FXML
+    private Hyperlink categories;
+
+    @FXML
+    private Hyperlink importt;
+
+    @FXML
+    private Hyperlink export;
+    
+    @FXML
+    private Tab tab1;
+
+    @FXML
+    private Tab tab2;
+
+    @FXML
+    private Tab tab3;
+
+    @FXML
+    private Tab tab4;
+
+    @FXML
+    private Spinner<?> tab2Click;
+
+    @FXML
+    private TreeView <?> tab2Popup;
+
+    @FXML
+    /// Khi bấm Create a New Question - 3.2
+    public void createQues(javafx.scene.input.MouseEvent mouseEvent) {
+        System.out.print("Test createQues");
+        try {
+            // Tạo một Stage mới
+            Stage stage = new Stage();
+
+            // Tải file FXML mới
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("gui3.2-create-question-view.fxml"));
+            Parent root = loader.load();
+
+            Gui32CreateQuestionViewController controller = loader.getController();
+            controller.setStage(stage);
+
+            // Tạo một Scene từ Parent và đặt nó cho Stage
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            // Hiển thị cửa sổ mới
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void showCategory(MouseEvent mouseEvent) {
+    }
     @Override
+    // 1.1 + 1.2 + 3.3
     public void initialize(URL url, ResourceBundle resourceBundle) {
         slider.setVisible(false);
         hoiPopup.setVisible(false);
         defaultPopup1.setVisible(false);
 
-        // Hiện popup question bank từ nút gear
+        // Hiện question bank từ nút gear
         Menu.setOnMouseClicked(event -> {
             list1.setVisible(false);
             slider.setVisible(true);
@@ -67,6 +118,7 @@ public class HelloController implements Initializable {
 
         // Hiện popup questions từ question bank
         questions.setOnMouseClicked(event -> {
+            hoiPopup.getSelectionModel().select(tab1);
             hoiPopup.setVisible(true);
             slider.setVisible(false);
         });
@@ -95,6 +147,49 @@ public class HelloController implements Initializable {
             hoiPopup.setVisible(false);
             Menu.setVisible(true);
             MenuBack.setVisible(false);
+        });
+
+        // Tab categories
+        tab2Popup.setVisible(false);
+
+        // Hiện popup categories từ question bank
+        categories.setOnMouseClicked(event -> {
+            hoiPopup.getSelectionModel().select(tab2);
+            hoiPopup.setVisible(true);
+            slider.setVisible(false);
+            tab2Popup.setVisible(false);
+        });
+
+        // Hiện popup tree
+        tab2Click.setOnMouseClicked(event -> {
+            if (!isTab2Popup[0]) {
+                tab2Popup.setVisible(true);
+                isTab2Popup[0] = true;
+            } else {
+                tab2Popup.setVisible(false);
+                isTab2Popup[0] = false;
+            }
+        });
+
+        // Ẩn popup tree khi click bên ngoài
+        tab2Popup.getParent().setOnMouseClicked(event -> {
+            if (!tab2Popup.getBoundsInParent().contains(event.getX(), event.getY())) {
+                tab2Popup.setVisible(false);
+            }
+        });
+
+        // Hiện popup importt từ question bank
+        importt.setOnMouseClicked(event -> {
+            hoiPopup.getSelectionModel().select(tab3);
+            hoiPopup.setVisible(true);
+            slider.setVisible(false);
+        });
+
+        // Hiện popup categories từ question bank
+        export.setOnMouseClicked(event -> {
+            hoiPopup.getSelectionModel().select(tab4);
+            hoiPopup.setVisible(true);
+            slider.setVisible(false);
         });
     }
 }
